d9f511a967c083199a9bb0b64db9f5878dab49b9
Ufuk Bakan <println.ufukbakan@gmail.com>
2022-04-11 18:29:45 +0300

    reinit

--- src/main/java/dev/ufuk/bakan/AracKiralamaPortaliApplication.java
+++ src/main/java/dev/ufuk/bakan/AracKiralamaPortaliApplication.java
@@ -1,4 +1,4 @@
-package dev.ufuk.bakan.arackiralamaportali;
+package dev.ufuk.bakan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/ufuk/bakan/AracKiralamaPortaliApplicationTests.java
+++ src/test/java/dev/ufuk/bakan/AracKiralamaPortaliApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.ufuk.bakan.arackiralamaportali;
+package dev.ufuk.bakan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
