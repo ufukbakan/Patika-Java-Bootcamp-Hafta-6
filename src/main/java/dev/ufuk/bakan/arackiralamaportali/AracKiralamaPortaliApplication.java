bfbb67aae097b77208a6ac27c96e4a0d6a64aee8
ufuk.bakan-p <ufuk.bakan-p@tubitak.gov.tr>
2022-04-11 18:09:03 +0300

    Initial commit

---
+++ src/main/java/dev/ufuk/bakan/arackiralamaportali/AracKiralamaPortaliApplication.java
@@ -0,0 +1,13 @@
+package dev.ufuk.bakan.arackiralamaportali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AracKiralamaPortaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AracKiralamaPortaliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ufuk/bakan/arackiralamaportali/AracKiralamaPortaliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ufuk.bakan.arackiralamaportali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AracKiralamaPortaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
